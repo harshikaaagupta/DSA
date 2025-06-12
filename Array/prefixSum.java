8563e29d98b71a8e355495d7680dd020f2d39759
Harshika Gupta <harshikagupta098@gmail.com>
2025-06-13 01:02:03 +0530

    initial commit

---
+++ Array/prefixSum.java
@@ -0,0 +1,27 @@
+public class prefixSum {
+    public static void prefixSum(int numbers[]){
+        int maxSum=Integer.MIN_VALUE;
+        int currSum=0;
+        int prefix[]=new int[numbers.length];
+        prefix[0]=numbers[0];
+        for(int i=1;i<prefix.length;i++){
+            prefix[i]=prefix[i-1]+numbers[i];
+        }
+        for(int i=0;i<numbers.length;i++){
+            int start=i;
+            for(int j=i;j<numbers.length;j++){
+                int end=j;
+                currSum=start == 0 ? prefix[end]:prefix[end]-prefix[start-1];
+                if(maxSum<currSum){
+                    maxSum=currSum;
+                }
+            }
+        }
+        System.out.println("the maximum sum is: "+maxSum);
+    }
+    public static void main(String[] args) {
+        int numbers[]={2,4,6,8,10};
+        prefixSum(numbers);
+    }
+    
+}

c470a483271e0c83c0e7807625dd3318b9c61be6
Harshika Gupta <harshikagupta098@gmail.com>
2025-06-13 19:33:59 +0530

    initial commit

---
+++ SortingAlgo/bubbleSort.java
@@ -0,0 +1,24 @@
+public class bubbleSort {
+    public static void bubbleSort(int arr[]){
+        for(int turn=0;turn<arr.length;turn++){
+            for(int j=0;j<arr.length-1-turn;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+    public static void printArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String args[]){
+        int arr[]={5,1,4,3,2};
+        bubbleSort(arr);
+        printArray(arr);
+
+    }
+}

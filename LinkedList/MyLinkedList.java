3695cdb73c29cd0aa3ea68ef039b39ccb12e0808
Harshika Gupta <harshikagupta098@gmail.com>
2025-07-21 19:16:24 +0530

    initial commit

--- LinkedList/MyLinkedList.java
+++ LinkedList/MyLinkedList.java
@@ -11,10 +11,12 @@ public class MyLinkedList {
     }
         public  Node head;
         public  Node tail;
+        public int size;
 
         public void addFirst(int data)
         {
             Node newNode = new Node(data);
+            size++;
             if(head == null){
                 head = tail = newNode;
                 return;
@@ -24,6 +26,7 @@ public class MyLinkedList {
         }
         public void addLast(int data){
             Node newNode = new Node(data);
+            size++;
             if(head == null){
                 head = tail = newNode;
                 return;
@@ -43,18 +46,91 @@ public class MyLinkedList {
             }
             System.out.println();
         }
+        public void add(int idx , int data){
+            if(idx == 0){
+                addFirst(data);
+                return;
+            }
+            Node newNode = new Node(data);
+            size++;
+            Node temp =head;
+            int i = 0;
+            while(i < idx-1){
+                temp = temp.next;
+                i++;
+            }
+            newNode.next = temp.next;
+            temp.next = newNode;
+        }
+
+        public int removeFirst(){
+            if(size == 0){
+                System.out.println("ll is empty.");
+                return Integer.MIN_VALUE;
+            }else if( size == 1){
+                int val = head.data;
+                head = tail = null;
+                size = 0;
+                return val;
+            }else{
+                int val = head.data;
+                head = head.next;
+                size -- ;
+                return val;
+            }
+        }
+
+        public int removeLast(){
+            if(size == 0){
+                System.out.println("ll is empty");
+                return Integer.MIN_VALUE;
+            }
+            else if(size == 1){
+                int val = head.data;
+                head = tail = null;
+                size = 0;
+                return val;
+            }
+            Node prev = head ;
+            for(int i = 0;i<size-2;i++){
+                prev = prev.next;
+            }
+            int val = prev.next.data;
+            prev.next = null;
+            tail = prev;
+            size --;
+            return val;
+        }
     
+        public int itrSearch(int key){
+            int i = 0;
+            Node temp = head;
+            while(temp != null){
+                if(temp.data == key){
+                    return i;
+                }
+                temp = temp.next;
+                i++;
+            }
+            return -1;
+        }
+
     public static void main(String[] args) {
         MyLinkedList ll = new MyLinkedList();
         ll.print();
         ll.addFirst(2);
-        ll.print();
         ll.addFirst(1);
-        ll.print();
-        ll.addLast(3);
-        ll.print();
         ll.addLast(4);
+        ll.addLast(5);
+        ll.add(2,3);
         ll.print();
+        ll.removeFirst();
+        ll.print();
+        ll.removeLast();
+        ll.print();
+        
+        System.out.println(ll.itrSearch(3));
+        System.out.println(ll.size);
     }
     
 }

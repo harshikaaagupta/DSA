494567f736a49944e01610271baaf1f44d57f33f
Harshika Gupta <harshikagupta098@gmail.com>
2025-08-27 00:30:44 +0530

    stackUsingQueues

---
+++ Queues/stackTwoQueues.java
@@ -0,0 +1,82 @@
+import java.util.*;
+
+public class stackTwoQueues {
+    static class Stcak {
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
+
+        public boolean isEmpty() {
+            return q1.isEmpty() && q2.isEmpty();
+        }
+
+        public void push(int data) {
+            if (!q1.isEmpty()) {
+                q1.add(data);
+            } else {
+                q2.add(data);
+            }
+        }
+
+        public int pop() {
+            if (isEmpty()) {
+                System.out.println("Empty stack");
+                return -1;
+            }
+            int top = -1;
+            // Case 1
+            if (!q1.isEmpty()) {
+                while (!q1.isEmpty()) {
+                    top = q1.remove();
+                    if (q1.isEmpty()) {
+                        break;
+                    }
+                    q2.add(top);
+                }
+            } else {
+                // Case 2
+                while (!q2.isEmpty()) {
+                    top = q2.remove();
+                    if (q2.isEmpty()) {
+                        break;
+                    }
+                    q1.add(top);
+                }
+            }
+            return top;
+        }
+
+        public int peek() {
+            if (isEmpty()) {
+                System.out.println("Empty stack");
+                return -1;
+            }
+            int top = -1;
+            // Case 1
+            if (!q1.isEmpty()) {
+                while (!q1.isEmpty()) {
+                    top = q1.remove();
+                    q2.add(top);
+                }
+            } else {
+                // Case 2
+                while (!q2.isEmpty()) {
+                    top = q2.remove();
+                    q1.add(top);
+                }
+            }
+            return top;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stcak s = new Stcak();   // use your custom stack class
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}

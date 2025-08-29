8dbb99b949f8238b630daab9ce421b60b2196637
Harshika Gupta <harshikagupta098@gmail.com>
2025-08-30 01:12:04 +0530

    Tree traversal

--- Binary Tree/BinaryTreeBuild.java
+++ Binary Tree/BinaryTreeBuild.java
@@ -24,12 +24,46 @@ public class BinaryTreeBuild {
 
             return newNode;
         }
+
+        public static void preOrder(Node root){
+            if(root == null){
+                return;
+            }
+            System.out.print(root.data+" ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+
+        public static void inOrder(Node root){
+            if(root == null){
+                return ;
+            }
+            inOrder(root.left);
+            System.out.print(root.data+" ");
+            inOrder(root.right);
+        }
+
+        public static void postOrder(Node root){
+            if(root == null){
+                return;
+            }
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.print(root.data+" ");
+        }
     }
     public static void main(String args[]){
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         System.out.println(root.data);
+
+        tree.preOrder(root);
+        System.out.println();
+        tree.inOrder(root);
+        System.out.println();
+        tree.postOrder(root);
+
 
     }
     

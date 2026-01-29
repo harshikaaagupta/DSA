1b5ba5e0668c78d26a2e7c2a3f2103b4a094284a
Harshika Gupta <harshikagupta098@gmail.com>
2026-01-30 00:14:55 +0530

    longest increasing subsequence

--- DynamicProgramming/lis.java
+++ DynamicProgramming/lis.java
@@ -7,12 +7,19 @@ public class lis{
         int dp[][] = new int[n+1][m+1];
 
         for(int i = 0;i<n+1;i++){
-            for(int j = 0 ;j<m+1;j++){
-                if(i == 0 || j == 0){
-                    dp[i][j] = 0;
-                }
-            }
+            dp[i][0] = 0;
         }
+        for(int j = 0;j<m+1;j++){
+            dp[0][j] = 0;
+        }
+
+        // for(int i = 0;i<n+1;i++){
+        //     for(int j = 0 ;j<m+1;j++){
+        //         if(i == 0 || j == 0){
+        //             dp[i][j] = 0;
+        //         }
+        //     }
+        // }
 
         for(int i = 1;i<n+1;i++){
             for(int j = 1;j<m+1;j++){

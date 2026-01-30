78ea94534dbb7baf51359480d2a97a1ffdc213d9
Harshika Gupta <harshikagupta098@gmail.com>
2026-01-30 23:03:04 +0530

    pattern matching

---
+++ DynamicProgramming/patternMatching.java
@@ -0,0 +1,44 @@
+public class patternMatching {
+    public static boolean patternMatching(String s , String p){
+        int n = s.length();
+        int m = p.length();
+        boolean dp[][] = new boolean[n+1][m+1];
+
+        //initialization
+        dp[0][0] = true;
+
+        //p = " "
+        for(int i = 1;i<n+1;i++){
+            dp[i][0] = false;
+        }
+
+        //s = " "
+        for(int j = 1;j<m+1;j++){
+            if(p.charAt(j-1) == '*'){
+                dp[0][j] = dp[0][j-1];
+            }
+        }
+
+        // bottom up (filling)
+
+        for(int i = 1;i<n+1;i++){
+            for(int j = 1;j<m+1;j++){
+                if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }else if(p.charAt(j-1) == '*'){
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }else{
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[n][m];
+
+    }
+    public static void main(String[] args) {
+        String str1 = "abcd";
+        String str2 = "abc*";
+
+        System.out.println(patternMatching(str1,str2));
+    }
+}

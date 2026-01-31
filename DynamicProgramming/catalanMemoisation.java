a43055242dcefd9df2129534dfc345c6a0b6d481
Harshika Gupta <harshikagupta098@gmail.com>
2026-01-31 23:40:04 +0530

    catalan memoisation

---
+++ DynamicProgramming/catalanMemoisation.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class catalanMemoisation {
+    public static int catalanMemoisation(int n , int dp[]){
+        if(n == 0 || n == 1){
+            return 1;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        int ans = 0;
+        for(int i = 0;i<n;i++){
+            ans += catalanMemoisation(i, dp)*catalanMemoisation(n-i-1, dp);
+        }
+        return dp[n] = ans;
+    }
+    public static void main(String args[]){
+        int n = 4;
+        int dp[] = new int[n+1];
+        Arrays.fill(dp,-1);
+
+        System.out.println(catalanMemoisation(n, dp));
+    }
+}

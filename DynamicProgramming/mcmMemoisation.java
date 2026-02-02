e8adeee16c1c35cbe4b808f52d48bf1161cf6baa
Harshika Gupta <harshikagupta098@gmail.com>
2026-02-03 00:21:03 +0530

    mcm memoisation

---
+++ DynamicProgramming/mcmMemoisation.java
@@ -0,0 +1,33 @@
+// Using Memoisation
+
+import java.util.*;
+public class mcmMemoisation {
+    public static int mcmMemoisation(int arr[],int i , int j , int dp[][]){
+        if(i == j){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int ans = Integer.MAX_VALUE;
+        for(int k = i ;k<= j-1 ;k++){
+            int cost1 = mcmMemoisation(arr, i, k, dp);
+            int cost2 = mcmMemoisation(arr, k+1, j, dp);
+            int cost3 = arr[i-1]*arr[k]*arr[j];
+            ans =Math.min(cost1+cost2+cost3,ans);
+
+        }
+        return dp[i][j] = ans;
+
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,3};
+        int n = arr.length;
+        int dp[][] = new int[n][n];
+        for(int i = 0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+
+        System.out.println(mcmMemoisation(arr, 1, n-1, dp));
+    }
+}

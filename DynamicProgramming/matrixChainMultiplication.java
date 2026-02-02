7335264c321387182555375e124cc06915867566
Harshika Gupta <harshikagupta098@gmail.com>
2026-02-02 23:38:46 +0530

    MCM Recursion

--- DynamicProgramming/matrixChainMultiplication.java
+++ DynamicProgramming/matrixChainMultiplication.java
@@ -1,6 +1,24 @@
 // recursion 
 public class matrixChainMultiplication {
+    public static int mcmRecursion(int arr[] , int i , int j){
+        if(i == j){ //single matrix
+            return 0;
+        }
+        int ans = Integer.MAX_VALUE;
+        for(int k = i;k<=j-1;k++){
+            int cost1= mcmRecursion(arr, i, k);
+            int cost2= mcmRecursion(arr,k+1, j);
+            int cost3 = arr[i-1]*arr[k]*arr[j];
+            int finalCost = cost1+cost2+cost3;
+            ans = Math.min(finalCost,ans);
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,3};
+        int n = arr.length;
+
+        System.out.println(mcmRecursion(arr, 1, n-1));
+
     }
 }

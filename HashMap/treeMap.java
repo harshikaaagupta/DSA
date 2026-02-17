ee18aaab34bdd567f2c02de6fa42174d2ecc6473
Harshika Gupta <harshikagupta098@gmail.com>
2026-02-17 13:50:09 +0530

    tree map

---
+++ HashMap/treeMap.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class treeMap {
+    public static void main(String[] args) {
+        TreeMap<String , Integer> tm = new TreeMap<>();
+        tm.put("India",200);
+        tm.put("USA",150);
+        tm.put("China",100);
+
+        System.out.println(tm);
+        
+    }
+}

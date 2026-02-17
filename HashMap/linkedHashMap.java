dffb1beac9ced6600a0eeb510f74eb91df042482
Harshika Gupta <harshikagupta098@gmail.com>
2026-02-17 13:45:22 +0530

    linked hashmap

---
+++ HashMap/linkedHashMap.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class linkedHashMap {
+    public static void main(String[] args) {
+        LinkedHashMap<String, Integer> lhm = new LinkedHashMap<>();
+        lhm.put("India",200);
+        lhm.put("USA",150);
+        lhm.put("China",100);
+
+        System.out.println(lhm);
+    }
+    
+}

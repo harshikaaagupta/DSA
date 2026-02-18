ae0ba63fcf44c49d80abb13c8053bfab8c990f63
Harshika Gupta <harshikagupta098@gmail.com>
2026-02-18 10:50:08 +0530

    linked hashset

---
+++ HashMap/linkedHashSet.java
@@ -0,0 +1,14 @@
+
+import java.util.LinkedHashSet;
+
+public class linkedHashSet {
+    public static void main(String[] args) {
+        LinkedHashSet<String> lhs = new LinkedHashSet<>();
+        lhs.add("Delhi");
+        lhs.add("Pune");
+        lhs.add("Dehradun");
+        lhs.add("Agra");
+
+        System.out.println(lhs);
+    }
+}

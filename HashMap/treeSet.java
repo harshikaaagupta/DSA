b070718dd40dea1fc1c90de77b8691d5750f046b
Harshika Gupta <harshikagupta098@gmail.com>
2026-02-18 11:29:56 +0530

    treeset

---
+++ HashMap/treeSet.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class treeSet {
+    public static void main(String[] args) {
+        TreeSet<String> ts = new TreeSet<>();
+        ts.add("Bengaluru");
+        ts.add("Delhi");
+        ts.add("Bombay");
+        ts.add("Noida");
+
+        System.out.println(ts);
+
+
+
+    }
+}

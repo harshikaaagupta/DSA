890cbe1eab653d97b4944a6c5703aa5b97bc4cbf
Harshika Gupta <harshikagupta098@gmail.com>
2026-02-24 16:29:58 +0530

    tree traversal

--- BinaryTrees/treeTraversal.java
+++ BinaryTrees/treeTraversal.java
@@ -1,4 +1,8 @@
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+
 public class treeTraversal {
     static class Node{
         int data;
@@ -56,6 +60,38 @@ public class treeTraversal {
         postOrder(root.right);
         System.out.print(root.data+" ");
     }
+
+    // LevelOrder Traversal
+    
+    public static void levelOrder(Node root){
+        if(root == null){
+            return;
+        }
+        Queue <Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while(!q.isEmpty()){
+            Node currNode = q.remove();
+            if(currNode == null){
+                System.out.println();
+
+                if(q.isEmpty()){
+                    break;
+                }else{
+                    q.add(null);
+                }
+            }else{
+                System.out.print(currNode.data+" ");
+                if(currNode.left != null){
+                    q.add(currNode.left);
+                }
+                if(currNode.right != null){
+                    q.add(currNode.right);
+                }
+            }
+        }
+    }
+
     }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
@@ -66,7 +102,9 @@ public class treeTraversal {
 
         // tree.inOrder(root);
 
-        tree.postOrder(root);
+        // tree.postOrder(root);
+
+        tree.levelOrder(root);
 
     }
     
